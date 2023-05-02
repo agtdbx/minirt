5773c26c104788dac8d40e102705e650d48f131d
Gugus <gugus.deroubaix@gmail.com>
2023-05-02 11:32:51 +0200

    Transparency full ok
    Shadow intensity is link on the transparency of the object which create this shadow

--- srcs/image/add_diffuse_intensity.c
+++ srcs/image/add_diffuse_intensity.c
@@ -6,13 +6,16 @@
 /*   By: aderouba <aderouba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 11:19:24 by aderouba          #+#    #+#             */
-/*   Updated: 2023/05/02 10:02:23 by aderouba         ###   ########.fr       */
+/*   Updated: 2023/05/02 11:32:38 by aderouba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-static bool	is_in_shadow(t_all *all, t_ray *ray, float distance, int id_ignore);
+static float	shadow_intersect(t_all *all, t_ray *ray, float distance,
+					int id_ignore);
+static void		manage_intersect(t_rtlst *obj, t_intersect_ret *res,
+					t_ray *ray);
 
 bool	add_diffuse_intensity(t_all *all, t_intersect_ret *res,
 			t_light const *light, t_vec3 const *pixel_pos)
@@ -35,36 +38,52 @@ bool	add_diffuse_intensity(t_all *all, t_intersect_ret *res,
 	angle_ratio = -vec3_dot_product(&res->nrm, &ray_from_light.direction);
 	if (angle_ratio < 0.0f)
 		return (false);
-	if (is_in_shadow(all, &ray_from_light, distance, res->id))
+	intensity = shadow_intersect(all, &ray_from_light, distance, res->id);
+	if (intensity <= 0.0f)
 		return (false);
 	distance_intensity = 1.0f - (distance / LIGHT_DIFFUSE_RADIUS);
-	intensity = angle_ratio * distance_intensity
-		* REFLECTION_DIFFUSE_RATIO;
+	intensity *= angle_ratio * distance_intensity * REFLECTION_DIFFUSE_RATIO;
 	incremente_intensity(res, light, intensity);
 	return (true);
 }
 
-static bool	is_in_shadow(t_all *all, t_ray *ray, float distance, int id_ignore)
+static float	shadow_intersect(t_all *all, t_ray *ray, float distance,
+					int id_ignore)
 {
 	t_intersect_ret	res;
 	t_rtlst			*obj;
+	float			transparency;
+	int				nb_intersect;
 
 	init_intersect_ret(&res, 0);
 	obj = all->scene.objects;
-	while (obj)
+	transparency = 1.0f;
+	nb_intersect = 0;
+	while (obj && transparency > 0.0f)
 	{
 		if (obj->id != id_ignore)
 		{
-			if (obj->type == SPHERE)
-				intersect_sphere(&obj->value.as_sphere, ray, &res);
-			else if (obj->type == PLANE)
-				intersect_plane(&obj->value.as_plane, ray, &res);
-			else if (obj->type == CYLINDER)
-				intersect_cylinder(&obj->value.as_cylinder, ray, &res);
+			manage_intersect(obj, &res, ray);
 			if (res.dst != -1.0f && res.dst < distance)
-				return (true);
+			{
+				transparency -= (1.0f - res.transparency_intensity);
+				nb_intersect++;
+			}
 		}
 		obj = obj->next;
 	}
-	return (res.dst != -1.0f && res.dst < distance);
+	if (nb_intersect == 0)
+		return (1.0f);
+	return (transparency);
+}
+
+static void	manage_intersect(t_rtlst *obj, t_intersect_ret *res, t_ray *ray)
+{
+	res->dst = -1.0f;
+	if (obj->type == SPHERE)
+		intersect_sphere(&obj->value.as_sphere, ray, res);
+	else if (obj->type == PLANE)
+		intersect_plane(&obj->value.as_plane, ray, res);
+	else if (obj->type == CYLINDER)
+		intersect_cylinder(&obj->value.as_cylinder, ray, res);
 }
